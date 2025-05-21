38d6ac5c8dafda06c67b973728c73b7c0218f293
Przemek <przemyslaw.przybylak2@gmail.com>
2025-05-21 22:37:20 +0200

    added task modal zystand store

---
+++ src/stores/useTaskModalStore.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+import { Task } from "@/types/task";
+
+interface TaskModalStore {
+  selectedTask: Task | null;
+  openTaskModal: (task: Task) => void;
+  closeTaskModal: () => void;
+}
+
+export const useTaskModalStore = create<TaskModalStore>((set) => ({
+  selectedTask: null,
+  openTaskModal: (task) => set({ selectedTask: task }),
+  closeTaskModal: () => set({ selectedTask: null }),
+}));
