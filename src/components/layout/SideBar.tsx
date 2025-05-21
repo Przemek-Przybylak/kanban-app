ee62b64f4b471498c2b19c6268f6f0e213c723d4
Przemek <przemyslaw.przybylak2@gmail.com>
2025-05-21 22:38:02 +0200

    basic SideBar added

---
+++ src/components/layout/SideBar.tsx
@@ -0,0 +1,22 @@
+export default function SideBar() {
+  return (
+    <div className=" bg-gray-200 p-4 flex-1">
+      <h2 className="text-xl font-bold mb-4 ">Side Bar</h2>
+      <ul>
+        <li className="mb-2">
+          <a href="#">Link 11b1b1b1bb1b11b1b1b11 1 1 1b1b1bb1b11 1 1 1 </a>
+        </li>
+        <li className="mb-2">
+          <a href="#">Link 2</a>
+        </li>
+        <li className="mb-2">
+          <a href="#">Link 3</a>
+        </li>
+        <li className="mb-2">
+          <a href="#">Link 4</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+1;
