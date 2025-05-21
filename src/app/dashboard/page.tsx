90d264082d22f36721b9001d5f6b1196022bca73
Przemek <przemyslaw.przybylak2@gmail.com>
2025-05-21 22:40:51 +0200

    task modal rendering added in Dashboard page file

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,15 @@
+"use client";
 import CanbanBoard from "@/components/kanban/CanbanBoard";
+import TaskModal from "@/components/modals/TaskModal";
 import { tasks } from "@/data/tasks";
+import { useTaskModalStore } from "@/stores/useTaskModalStore";
 
 export default function DashboardPage() {
+  const { selectedTask } = useTaskModalStore();
+  console.log(selectedTask);
   return (
-    <div className="p-6">
+    <div className="p-6 flex-3">
+      {selectedTask && <TaskModal />}
       <CanbanBoard tasks={tasks} />
     </div>
   );
