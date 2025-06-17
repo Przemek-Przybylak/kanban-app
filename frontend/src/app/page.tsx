268fc0dabcf7de96a4fc2de2c3a0c35f5a92617e
Przemek <przemyslaw.przybylak2@gmail.com>
2025-06-17 05:19:58 +0200

    use Button component in homepgae

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,10 @@
 import { redirect } from "next/navigation";
+import Button from "../components/Button/Button";
 
 export default function Home() {
-  redirect("/dashboard");
+  return (
+    <div className="p-6 flex-3">
+      <Button>Add project</Button>
+    </div>
+  );
 }
