9430a95139167d3159fa31b618e24852695ff877
Przemek <przemyslaw.przybylak2@gmail.com>
2025-06-20 13:26:27 +0200

    use dashboard in project page

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 import CanbanBoard from "../../components/kanban/CanbanBoard";
 import TaskModal from "../../components/modals/TaskModal";
-import { projects } from "../../data/projects";
 import { useTaskModalStore } from "../../stores/useTaskModalStore";
+import { Project } from "../../types/projects";
 
-export default function DashboardPage() {
+export default function DashboardPage({ project }: { project: Project }) {
   const { selectedTask } = useTaskModalStore();
-  console.log(selectedTask);
+
   return (
     <div className="p-6 flex-3">
       {selectedTask && <TaskModal />}
-      <CanbanBoard tasks={projects[0].tasks} />
+      <CanbanBoard tasks={project.tasks} />
     </div>
   );
 }
