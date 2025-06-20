89fcb10e3c2bc8ca65a0fcb537ac520ee654649c
Przemek <przemyslaw.przybylak2@gmail.com>
2025-06-20 12:17:27 +0200

    cors installed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import projectsRouter from "./routes/projects";
+import cors from "cors";
 
 const app = express();
 const PORT = 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/projects", projectsRouter);
 
