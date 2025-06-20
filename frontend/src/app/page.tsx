7c16154d1f4b275ca3509f371db19b0b5fc27cd9
Przemek <przemyslaw.przybylak2@gmail.com>
2025-06-20 20:09:31 +0200

    code refactoring...

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,3 @@
-import Button from "../components/Button/Button";
-
 export default function Home() {
-  return (
-    <div className="">
-      <Button>Add project</Button>
-    </div>
-  );
+  return <div className="">hello world</div>;
 }
