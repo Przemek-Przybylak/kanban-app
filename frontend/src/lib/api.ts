7ac1ffaf3f0d9f337325b01ebdddb16764e64aa4
Przemek <przemyslaw.przybylak2@gmail.com>
2025-06-20 21:19:31 +0200

    added deploy backend adress to fetch data

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,5 @@
 export async function fetchProjects() {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/projects`
-  );
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/projects`);
   if (!response.ok) throw new Error("Failed to fetch projects");
 
   return response.json();
@@ -9,7 +7,7 @@ export async function fetchProjects() {
 
 export const fetchProject = async (id: number) => {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/projects/${id}`
+    `${process.env.NEXT_PUBLIC_API_URL}/projects/${id}`
   );
   if (!response.ok) throw new Error("Failed to fetch project");
   return response.json();
