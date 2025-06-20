9423a124c8db5f9795e718418a8411bd69f91694
Przemek <przemyslaw.przybylak2@gmail.com>
2025-06-20 13:23:55 +0200

    added one project to zustand store

--- frontend/src/stores/useProjectsStore.ts
+++ frontend/src/stores/useProjectsStore.ts
@@ -1,15 +1,18 @@
 import { create } from "zustand";
 import { Project } from "../types/projects";
-import { fetchProjects } from "../lib/api";
+import { fetchProject, fetchProjects } from "../lib/api";
 
 interface ProjectsStore {
+  project: Project | null;
   projects: Project[];
   error: string | null;
   loading: boolean;
   fetchProjects: () => Promise<void>;
+  fetchProject: (id: number) => Promise<void>;
 }
 
 export const useProjectsStore = create<ProjectsStore>((set) => ({
+  project: null,
   projects: [],
   error: null,
   loading: false,
@@ -19,6 +22,15 @@ export const useProjectsStore = create<ProjectsStore>((set) => ({
     try {
       const projects: Project[] = await fetchProjects();
       set({ projects, loading: false });
+    } catch (error) {
+      set({ error: (error as Error).message, loading: false });
+    }
+  },
+  fetchProject: async (id: number) => {
+    set({ loading: true, error: null });
+    try {
+      const response = await fetchProject(id);
+      set({ project: response, loading: false });
     } catch (error) {
       set({ error: (error as Error).message, loading: false });
     }
