7f59360cf7287349f430f7ab0fb1d3e1549f600b
Przemek <przemyslaw.przybylak2@gmail.com>
2025-06-20 21:26:56 +0200

    cors credetials for frontend added in index backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,12 @@ import cors from "cors";
 const app = express();
 const PORT = 3001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://kanban-app-sage-eta.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/projects", projectsRouter);
 
