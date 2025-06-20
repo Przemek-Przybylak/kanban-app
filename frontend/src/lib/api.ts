68d6fd15b3a90017c5ec6307f6cefe3702170316
Przemek <przemyslaw.przybylak2@gmail.com>
2025-06-20 11:55:39 +0200

    api route added for fetcher link

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,7 @@
 export async function fetchProjects() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/projects`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/projects`
+  );
   if (!response.ok) throw new Error("Failed to fetch projects");
 
   return response.json();
