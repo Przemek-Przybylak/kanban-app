89aaf5b6c6719bc976cb2a33a7317e5a9059f491
Przemek <przemyslaw.przybylak2@gmail.com>
2025-06-20 13:25:05 +0200

    project page component added

--- frontend/src/app/project/[id]/page.tsx
+++ frontend/src/app/project/[id]/page.tsx
@@ -1,8 +1,24 @@
+"use client";
+
+import { use, useEffect } from "react";
+import { useProjectsStore } from "../../../stores/useProjectsStore";
+import { useParams } from "next/navigation";
+import DashboardPage from "../../dashboard/page";
+
 export default function ProjectPage() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold mb-4">Project Page</h1>
-      <p className="text-gray-600">This is the project page.</p>
-    </div>
-  );
+  const { fetchProject, project, loading, error } = useProjectsStore();
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      const numericId = Number(id);
+      if (!isNaN(numericId)) {
+        fetchProject(numericId);
+      }
+    }
+  }, [id, fetchProject]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  return <>{project && <DashboardPage project={project} />}</>;
 }
