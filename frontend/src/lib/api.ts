8208603a8f36b8073603f568848225a121b9dfb1
Przemek <przemyslaw.przybylak2@gmail.com>
2025-06-22 11:06:37 +0200

    send porjct function added

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,3 +1,5 @@
+import { Project } from "../types/projects";
+
 export async function fetchProjects() {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/projects`);
   if (!response.ok) throw new Error("Failed to fetch projects");
@@ -10,5 +12,18 @@ export const fetchProject = async (id: number) => {
     `${process.env.NEXT_PUBLIC_API_URL}/projects/${id}`
   );
   if (!response.ok) throw new Error("Failed to fetch project");
+  return response.json();
+};
+
+export const sendProject = async (project: Project) => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/projects`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(project),
+  });
+
+  if (!response.ok) throw new Error("Failed to add project");
   return response.json();
 };
