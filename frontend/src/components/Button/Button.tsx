c39823716ae91ecb86fdc7ed6c92ad644eabe86c
Przemek <przemyslaw.przybylak2@gmail.com>
2025-06-29 14:38:47 +0200

    max-h for buttton added

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -10,7 +10,7 @@ export default function Button({
   return (
     <button
       onClick={onClick}
-      className={`bg-gray-600 text-white px-4 py-2 rounded hover:bg-gray-400 transition-colors mb-4 uppercase ${className}`}
+      className={`bg-gray-600 text-white px-4 py-2 rounded hover:bg-gray-400 transition-colors mb-4 uppercase max-h-[40px] ${className}`}
     >
       {children}
     </button>
