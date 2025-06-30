a8c9a828c285dec3cc06787dc950fec1c9fd6270
Przemek <przemyslaw.przybylak2@gmail.com>
2025-06-30 20:19:44 +0200

    add task button added in Canban Colmn component

--- frontend/src/components/kanban/CanbanColumn.tsx
+++ frontend/src/components/kanban/CanbanColumn.tsx
@@ -1,3 +1,4 @@
+import { useModalStore } from "../../stores/useModalStore";
 import { Task } from "../../types/task";
 import Button from "../Button/Button";
 import TaskCard from "./TaskCard";
@@ -5,13 +6,14 @@ import TaskCard from "./TaskCard";
 export default function CanbanColumn({ tasks }: { tasks: Task[] }) {
   const status = tasks.length > 0 ? tasks[0].status : "No tasks";
   const formattedStatus = status.replace(/_/g, " ").toUpperCase();
+  const { openModal } = useModalStore();
 
   return (
     <div className="bg-gray-100 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 flex flex-col">
       <h3 className="text-xl font-semibold mb-4 text-gray-700 border-b border-gray-300 pb-2">
         {formattedStatus}
       </h3>
-      <Button onClick={() => console.log("Add task clicked")}>Add Task</Button>
+      <Button onClick={() => openModal("editProject")}>Add Task</Button>
       <div className="flex flex-col gap-4 overflow-y-auto max-h-[70vh]">
         {tasks.length > 0 ? (
           tasks.map((task) => <TaskCard key={task.id} task={task} />)
