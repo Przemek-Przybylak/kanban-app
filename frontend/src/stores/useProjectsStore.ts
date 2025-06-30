6f48a58b7b00a32b045e1888efaf0adfb254debe
Przemek <przemyslaw.przybylak2@gmail.com>
2025-06-30 20:21:28 +0200

    id type changed from number to string in fetch function, update project function added

--- frontend/src/stores/useProjectsStore.ts
+++ frontend/src/stores/useProjectsStore.ts
@@ -1,6 +1,11 @@
 import { create } from "zustand";
 import { Project } from "../types/projects";
-import { fetchProject, fetchProjects, postProject } from "../lib/api";
+import {
+  fetchProject,
+  fetchProjects,
+  postProject,
+  putProject,
+} from "../lib/api";
 
 interface ProjectsStore {
   project: Project | null;
@@ -8,8 +13,9 @@ interface ProjectsStore {
   error: string | null;
   loading: boolean;
   fetchProjects: () => Promise<void>;
-  fetchProject: (id: number) => Promise<void>;
+  fetchProject: (id: string) => Promise<void>;
   sendProject: (project: Project) => Promise<void>;
+  updateProject: (id: string, project: Project) => Promise<void>;
 }
 
 export const useProjectsStore = create<ProjectsStore>((set) => ({
@@ -27,7 +33,7 @@ export const useProjectsStore = create<ProjectsStore>((set) => ({
       set({ error: (error as Error).message, loading: false });
     }
   },
-  fetchProject: async (id: number) => {
+  fetchProject: async (id: string) => {
     set({ loading: true, error: null });
     try {
       const response = await fetchProject(id);
@@ -42,6 +48,18 @@ export const useProjectsStore = create<ProjectsStore>((set) => ({
       await postProject(project);
       set((state) => ({
         projects: [...state.projects, project],
+        loading: false,
+      }));
+    } catch (error) {
+      set({ error: (error as Error).message, loading: false });
+    }
+  },
+  updateProject: async (id: string, project: Project) => {
+    set({ loading: true, error: null });
+    try {
+      await putProject(id, project);
+      set((state) => ({
+        projects: state.projects.map((p) => (p.projectId === id ? project : p)),
         loading: false,
       }));
     } catch (error) {
