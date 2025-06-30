eae16cb65291e3f7fb040ba6a46b098374df80dc
Przemek <przemyslaw.przybylak2@gmail.com>
2025-06-30 20:19:06 +0200

    usa add and edit project components into ProjectPage

--- frontend/src/app/project/[id]/page.tsx
+++ frontend/src/app/project/[id]/page.tsx
@@ -5,15 +5,19 @@ import { useProjectsStore } from "../../../stores/useProjectsStore";
 import { useParams } from "next/navigation";
 import StatusWrapper from "../../../components/StatusWrapper/StatusWrapper";
 import Dashboard from "../../../components/Dashboard/Dashboard";
+import { AddProjectModal } from "../../../components/modals/AddProjectModal";
+import EditProjectModal from "../../../components/modals/EditProjectModal";
+import { useModalStore } from "../../../stores/useModalStore";
 
 export default function ProjectPage() {
+  const { type, openModal } = useModalStore();
   const { fetchProject, project, loading, error } = useProjectsStore();
   const { id } = useParams();
 
   useEffect(() => {
     if (id) {
-      const numericId = Number(id);
-      if (!isNaN(numericId)) {
+      const numericId = String(id);
+      if (!isNaN(Number(numericId))) {
         fetchProject(numericId);
       }
     }
@@ -22,6 +26,8 @@ export default function ProjectPage() {
   return (
     <>
       <StatusWrapper loading={loading} error={error}>
+        {type === "addProject" && <AddProjectModal />}
+        {type === "editProject" && <EditProjectModal />}
         {project && <Dashboard project={project} />}
       </StatusWrapper>
     </>
