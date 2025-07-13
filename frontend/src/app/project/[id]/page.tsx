9fb1c1b5b30dd6696d2e63de2f5729e4d970f038
Przemek <przemyslaw.przybylak2@gmail.com>
2025-07-13 20:42:38 +0200

    implement fetch tasks by project to Project Page component added

--- frontend/src/app/project/[id]/page.tsx
+++ frontend/src/app/project/[id]/page.tsx
@@ -1,29 +1,29 @@
 "use client";
 
 import { useEffect } from "react";
-import { useProjectsStore } from "../../../stores/useProjectsStore";
 import { useParams } from "next/navigation";
 import StatusWrapper from "../../../components/StatusWrapper/StatusWrapper";
 import Dashboard from "../../../components/Dashboard/Dashboard";
 import { AddProjectModal } from "../../../components/modals/AddProjectModal";
 import EditProjectModal from "../../../components/modals/EditProjectModal";
 import { useModalStore } from "../../../stores/useModalStore";
+import { useTasksStore } from "../../../stores/useTasksStore";
 
 export default function ProjectPage() {
   const { type } = useModalStore();
-  const { fetchProject, project, loading, error } = useProjectsStore();
+  const { fetchTasksByProjectId, tasks, loading, error } = useTasksStore();
   const { id } = useParams();
 
   useEffect(() => {
-    fetchProject(id as string);
-  }, [id, fetchProject]);
+    fetchTasksByProjectId(id as string);
+  }, [id, fetchTasksByProjectId]);
 
   return (
     <>
       <StatusWrapper loading={loading} error={error}>
         {type === "addProject" && <AddProjectModal />}
         {type === "editProject" && <EditProjectModal />}
-        {project && <Dashboard project={project} />}
+        {tasks && <Dashboard tasks={tasks} />}
       </StatusWrapper>
     </>
   );
