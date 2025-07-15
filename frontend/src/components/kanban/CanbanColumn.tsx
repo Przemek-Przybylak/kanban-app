ab693a185ec27f546d8808e4a59f67a41e1928ec
Przemek <przemyslaw.przybylak2@gmail.com>
2025-07-15 21:25:39 +0200

    rename id into task id ni canbanColumn component

--- frontend/src/components/kanban/CanbanColumn.tsx
+++ frontend/src/components/kanban/CanbanColumn.tsx
@@ -16,7 +16,7 @@ export default function CanbanColumn({ tasks }: { tasks: Task[] }) {
       <Button onClick={() => openModal("addTask")}>Add Task</Button>
       <div className="flex flex-col gap-4 overflow-y-auto max-h-[70vh]">
         {tasks.length > 0 ? (
-          tasks.map((task) => <TaskCard key={task.id} task={task} />)
+          tasks.map((task) => <TaskCard key={task.taskId} task={task} />)
         ) : (
           <p className="text-gray-500 italic">Brak zadań w tej kolumnie</p>
         )}
