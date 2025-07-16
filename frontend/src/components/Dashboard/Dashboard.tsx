52d5f7094856bbb615bc2b1284c129028f5592b9
Przemek <przemyslaw.przybylak2@gmail.com>
2025-07-16 12:54:47 +0200

    styles for m  and s screens added in Dashboard

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -5,16 +5,17 @@ import TaskModal from "../../components/modals/TaskModal";
 import { useModalStore } from "../../stores/useModalStore";
 import { Task } from "../../types/task";
 
-type props = {
+type Props = {
   tasks: Task[];
 };
 
-export default function Dashboard({ tasks }: props) {
-  const { type } = useModalStore();
+export default function Dashboard({ tasks }: Props) {
+  const { type, isOpen } = useModalStore();
 
   return (
-    <div className="p-6 flex-3">
-      {type === "task" && <TaskModal />}
+    <div className="p-4 md:p-6 lg:p-8 flex-1 overflow-x-auto">
+      {isOpen && type === "task" && <TaskModal />}
+
       <CanbanBoard tasks={tasks} />
     </div>
   );
