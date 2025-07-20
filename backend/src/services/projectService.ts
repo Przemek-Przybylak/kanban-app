3b95959938932015227866cbde24e868024c87b3
Przemek <przemyslaw.przybylak2@gmail.com>
2025-07-20 10:43:55 +0200

    project service added

---
+++ backend/src/services/projectService.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from "@prisma/client";
+import { Project } from "../types/projects";
+
+const prisma = new PrismaClient();
+
+export async function getProject(projectId: string) {
+  return await prisma.project.findUnique({
+    where: { projectId },
+  });
+}
+
+export async function getProjects() {
+  return await prisma.project.findMany();
+}
+
+export async function createProject(project: Project) {
+  console.log("Creating project:", project);
+  return await prisma.project.create({
+    data: {
+      title: project.title,
+    },
+  });
+}
+
+export async function deleteProject(projectId: string) {
+  console.log("Deleting project with ID:", projectId);
+  return await prisma.project.delete({
+    where: { projectId },
+  });
+}
+
+export async function editProject(
+  projectId: string,
+  newData: Partial<Project>
+) {
+  const { title } = newData;
+
+  return await prisma.project.update({
+    where: { projectId },
+    data: {
+      ...(title && { title }),
+    },
+  });
+}
