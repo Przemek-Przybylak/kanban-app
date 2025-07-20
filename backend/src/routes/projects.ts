b0c60db341c758738aa1f0a3644f5b8733b10d21
Przemek <przemyslaw.przybylak2@gmail.com>
2025-07-20 10:43:40 +0200

    dynamic project from database send to frontend implement in controller

--- backend/src/routes/projects.ts
+++ backend/src/routes/projects.ts
@@ -1,16 +1,16 @@
 import express from "express";
-import { Project, projects } from "../data/projects";
-import { v4 as uuidv4 } from "uuid";
+import * as projectController from "../controllers/projectControllers";
 
 const projectsRouter = express.Router();
 
-projectsRouter.get("/", (req, res) => {
+projectsRouter.get("/", async (req, res) => {
+  const projects = await projectController.getProjectsController();
   res.json(projects);
 });
 
-projectsRouter.get("/:id", (req, res) => {
-  const id = req.params.id;
-  const project = projects.find((p) => p.projectId === id);
+projectsRouter.get("/:id", async (req, res) => {
+  const projectId = req.params.id;
+  const project = await projectController.getProjectController({ projectId });
   if (project) {
     res.json(project);
   } else {
@@ -18,36 +18,43 @@ projectsRouter.get("/:id", (req, res) => {
   }
 });
 
-projectsRouter.post("/", (req, res) => {
-  const newProject = req.body as Project;
+projectsRouter.post("/", async (req, res) => {
+  const newProject = req.body;
+
   if (!newProject.title) {
     res.status(400).json({ error: "Name are required" });
     return;
   }
-  newProject.projectId = uuidv4();
-  projects.push(newProject);
-  res.status(201).json(newProject);
+  try {
+    const createdProject = await projectController.createProjectController(
+      newProject
+    );
+    res.status(201).json(createdProject);
+  } catch (error) {
+    console.error("Error creating project:", error);
+    res.status(500).json({ error: "Failed to create project" });
+  }
 });
 
-projectsRouter.put("/:id", (req, res) => {
+projectsRouter.put("/:id", async (req, res) => {
   // console.log("PUT /projects/:id - BODY:", req.body); //
-  const id = req.params.id;
-  const updatedProject = req.body as Project;
-  const index = projects.findIndex((p) => p.projectId === id);
-  if (index >= 0) {
-    projects[index] = { ...projects[index], ...updatedProject };
-    // console.log("Backend wysyła:", projects[index]);
-    res.status(200).json(projects[index]);
+  const projectId = req.params.id;
+  const newDatat = req.body;
+  const updatedProject = await projectController.editProjectController(
+    projectId,
+    newDatat
+  );
+  if (updatedProject) {
+    res.status(200).json(updatedProject);
   } else {
     res.status(404).json({ error: "Project not found" });
   }
 });
 
-projectsRouter.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  const index = projects.findIndex((p) => p.projectId === id);
-  if (index >= 0) {
-    projects.splice(index, 1);
+projectsRouter.delete("/:id", async (req, res) => {
+  const projectId = req.params.id;
+  const deleted = await projectController.deleteProjectController(projectId);
+  if (deleted) {
     res.status(204).send();
   } else {
     res.status(404).json({ error: "Project not found" });
