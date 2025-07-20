58ade7a31af290e102c55772fdbfe96e45a871d4
Przemek <przemyslaw.przybylak2@gmail.com>
2025-07-20 10:42:47 +0200

    comment mock project

--- backend/src/data/projects.ts
+++ backend/src/data/projects.ts
@@ -1,13 +1,13 @@
-export interface Project {
-  projectId: string; // Changed to string for UUID compatibility
-  title: string;
-  tasksId: string[];
-}
+// export interface Project {
+//   projectId: string; // Changed to string for UUID compatibility
+//   title: string;
+//   tasksId: string[];
+// }
 
-export const projects: Project[] = [
-  {
-    title: "Learn coding",
-    projectId: "8528525b-cded-4ecd-94b1-52ad5998d5f6",
-    tasksId: [],
-  },
-];
+// export const projects: Project[] = [
+//   {
+//     title: "Learn coding",
+//     projectId: "8528525b-cded-4ecd-94b1-52ad5998d5f6",
+//     tasksId: [],
+//   },
+// ];
