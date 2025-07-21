e1700c25c6dfa10e354277b010d963531d0a6f40
Przemek <przemyslaw.przybylak2@gmail.com>
2025-07-21 05:38:37 +0200

    added url for production in prisma client helper

--- backend/src/lib/prismaClient.ts
+++ backend/src/lib/prismaClient.ts
@@ -3,7 +3,10 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_URL,
+      url:
+        process.env.NODE_ENV === "production"
+          ? process.env.DATABASE_URL_PROD
+          : process.env.DATABASE_URL,
     },
   },
 });
